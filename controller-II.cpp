1c21402e4981ebf3265b597e1c6491be2dcbd8e3
LiaoZiqi-GZFLS <503546675@qq.com>
2025-02-05 23:20:10 +0800

    Load libs

--- controller-II.cpp
+++ controller-II.cpp
@@ -8,32 +8,33 @@
 
 int main(int argc, char* argv[]){
     using namespace std;
-    colorDemo();
-    if(!IsProcessRunAsAdmin()){
-        SetConsoleColor(GREEN);
-        printf("[User Mode]\n");
-        SetConsoleColor(LIGHT_GREEN);
-        printf("[User Mode]\n");
-    }else{
-        SetConsoleColor(RED);
-        printf("[Administrator  Mode]\n");
-        SetConsoleColor(DEFAULT);
-    }
     if(argc==1){
+        if(!IsProcessRunAsAdmin()){
+            SetConsoleColor(GREEN);
+            printf("[User Mode]\n");
+        }else{
+            SetConsoleColor(RED);
+            printf("[Administrator  Mode]\n");
+            SetConsoleColor(DEFAULT);
+        }
+        system("cls");
         SetConsoleColor(DEFAULT);
         printf("Controller (II) [Version 1.2.0.0a]\n");
         SetConsoleColor(CYAN);
         printf("(c) Lazybones LZQ Corporation. ");
         SetConsoleColor(DEFAULT);
         printf("All rights reserved.\n");
+        /*------------------------------*/
+
+        /*------------------------------*/
+        SetConsoleColor(GREEN);
+        printf("Programe exited. ");
+        SetConsoleColor(DEFAULT);
+        printf("(Press any key to continue)\n");
+        system("pause>nul");
     }else{
         SetConsoleColor(DEFAULT);
         
     }
-    SetConsoleColor(GREEN);
-    printf("Programe exited. ");
-    SetConsoleColor(DEFAULT);
-    printf("(Press any key to continue)\n");
-    system("pause>nul");
     return 0;
 }
